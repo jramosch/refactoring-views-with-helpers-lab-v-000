20bdbfa60f308cf1ca564d2393bee6c3632aca12
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-10-15 19:59:11 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
     if song.artist.present?
       link_to song.artist_name, artist_path(song.artist)
     else
-      link_to "Add Artist", 
+      link_to "Add Artist", 'artists/#{song.artist.id}/edit'
     end
   end
 end
